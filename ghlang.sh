82e9f444befca25ce02c0ac9c4d3db0980cde393
Doggo <fearlessdoggo21@vivaldi.net>
2022-03-31 23:36:23 -0500

    bring ghlang.sh upto date

--- ghlang.sh
+++ ghlang.sh
@@ -1,27 +1,34 @@
 #!/bin/sh
 # ghlang - get github repository languages
-# Copyright (C) 2022 FearlessDoggo21
+# Copyright (C) 2022 ArcNyxx
 # see LICENCE file for licensing information
 
-[ -z "$NAME" ] && echo "ghlang: set \$NAME to github username" && exit 1
-
-AUTH="-H \"Authorization: token $TOKEN\""
-[ -z "$TOKEN" ] && AUTH=""
+echo -n 'github: enter username: ' >&2
+read -r NAME
 
 TEMP=$(mktemp -dt)
 trap "rm -rf $TEMP" EXIT
 
-while read -r REPO; do
-	URL="https://api.github.com/repos/${NAME}/${REPO}/languages"
-	curl "$URL" $AUTH 2> /dev/null | grep '^  ' | tr -d ':,' |
-		while read -r LANG; do
-			DATA=$(echo "$LANG" | cut '-d ' -f2)
-			LANG=$(echo "$LANG" | cut '-d ' -f1 | cut '-d"' -f2)
+[ -n "$GHTOKEN" ] && AUTH="-H \"Authorization: token $GHTOKEN\""
 
-			# no handling for lack of file, just equal to 0
-			VALUE=$(cat "$TEMP/$LANG" 2> /dev/null)
-			echo $((VALUE + "$DATA")) > "$TEMP/$LANG"
-		done
+PAGE='1'
+while true; do
+	URL="https://api.github.com/users/${NAME}/repos?per_page=100&page=${PAGE}"
+	REPOS="$(curl "$URL" $AUTH 2>/dev/null | grep '^    "name":' | cut '-d"' -f4) $REPOS"
+	[ "$(echo "$REPOS" | wc -w)" != "$((PAGE * 100))" ] && break
+	PAGE=$((PAGE + 1))
+done
+
+echo "$REPOS" | while read -r REPO; do
+	URL="https://api.github.com/repos/${NAME}/${REPO}/languages"
+	curl "$URL" $AUTH 2>/dev/null | grep '^  ' | tr -d ':,' | while read -r LANG; do
+		DATA=$(echo "$LANG" | cut '-d ' -f2)
+		LANG=$(echo "$LANG" | cut '-d ' -f1 | cut '-d"' -f2)
+
+		# no handling for lack of file, just equal to 0
+		VALUE=$(cat "$TEMP/$LANG" 2> /dev/null)
+		echo $((VALUE + "$DATA")) > "$TEMP/$LANG"
+	done
 done
 
 ls "$TEMP" | while read -r LANG; do
