91d6f1a3261b3211e1b33298f4771bb9c1e3c054
Doggo <fearlessdoggo21@vivaldi.net>
2022-04-03 00:27:19 -0500

    update ghlang.sh slightly

--- ghlang.sh
+++ ghlang.sh
@@ -11,10 +11,7 @@ fi
 TEMP=$(mktemp -dt)
 trap "rm -rf $TEMP" EXIT
 
-AUTH="-H \"Authorization: token $GHTOKEN\""=
-[ -z "$GHTOKEN" ] && AUTH=
-
-PAGE='1'
+PAGE='1'; AUTH="${GHTOKEN:+--oauth2-bearer "$GHTOKEN"}"
 while true; do
 	URL="https://api.github.com/users/$1/repos?per_page=100&page=${PAGE}"
 	REPOS="$(curl -s "$URL" $AUTH | grep '^    "name":' | cut '-d"' -f4) $REPOS"
@@ -24,7 +21,7 @@ done
 
 echo "$REPOS" | while read -r REPO; do
 	URL="https://api.github.com/repos/${NAME}/${REPO}/languages"
-	curl -s "$URL" $AUTH | grep '^  ' | tr -d ':,' | while read -r LANG; do
+	curl -s "$URL" | grep '^  ' | tr -d ':,' | while read -r LANG; do
 		DATA=$(echo "$LANG" | cut '-d ' -f2)
 		LANG=$(echo "$LANG" | cut '-d ' -f1 | cut '-d"' -f2)
 
