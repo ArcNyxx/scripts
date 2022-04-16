28eefe82e154336ca32f8444687a5471f041bcf7
Doggo <fearlessdoggo21@vivaldi.net>
2022-04-16 18:42:48 -0500

    update ghlang

--- error.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# error - error function
-# Copyright (C) 2022 ArcNyxx
-# see LICENCE file for licensing information
-
-error() {
-	echo "$1"; exit ${2:-1}
-}
--- ghlang.sh
+++ ghlang.sh
@@ -3,9 +3,9 @@
 # Copyright (C) 2022 ArcNyxx
 # see LICENCE file for licensing information
 
-. /usr/local/etc/error.sh
-
-[ $# -ne 1 ] && error 'usage: ghlang [name]'
+if [ $# -ne 1 ]; then
+	echo 'usage: ghlang [name]' >&2; exit 1
+fi
 
 TEMP=$(mktemp -dt)
 trap "rm -rf $TEMP" EXIT
@@ -13,15 +13,18 @@ trap "rm -rf $TEMP" EXIT
 PAGE='1'; AUTH="${GHTOKEN:+--oauth2-bearer "$GHTOKEN"}"
 while true; do
 	URL="https://api.github.com/users/$1/repos?per_page=100&page=$PAGE"
-	curl -s $AUTH "$URL" | grep '^    "name":' | cut '-d"' -f4
-	[ "$(echo "$REPOS" | wc -w)" -ne "$((PAGE * 100))" ] && break
+	RE="$RE $(curl -s $AUTH "$URL" | grep '^    "name":' | cut '-d"' -f4)"
+
+	[ "$(echo "$RE" | wc -w)" -ne "$((PAGE * 100))" ] && break
 	PAGE=$((PAGE + 1))
-done | while read -r REPO; do
-	URL="https://api.github.com/repos/$1/$REPO/languages"
-	curl -s $AUTH "$URL" | grep '^  ' | tr -d ' ",' | while read -r IN; do
-		VALUE="$(cat "$TEMP/${IN%:*}" 2>/dev/null)"
-		echo $((VALUE + ${IN#*:})) >"$TEMP/${IN%:*}"
-	done
+done
+
+echo "$RE" | while read -r REPO; do
+	curl -s $AUTH "https://api.github.com/repos/$1/$REPO/languages" |
+		grep '^  ' | tr -d ' ",' | while read -r IN; do
+			VALUE="$(cat "$TEMP/${IN%:*}" 2>/dev/null)"
+			echo $((VALUE + ${IN#*:})) >"$TEMP/${IN%:*}"
+		done
 done
 
 ls "$TEMP" | while read -r LANG; do
