7abe270ef57bf21920c85e89d29512cde5ae763d
Doggo <fearlessdoggo21@vivaldi.net>
2022-04-16 22:50:26 -0500

    update print.sh

--- print.sh
+++ print.sh
@@ -3,40 +3,31 @@
 # Copyright (C) 2022 ArcNyxx
 # see LICENCE file for licensing information
 
-. /usr/local/etc/error.sh
+error() {
+	echo $1 >&2; exit ${2:-1}
+}
 
 [ $# -eq 0 ] && error 'usage: print [cmd] [opt...]'
+if [ "$1" = 'stat' ]; then
+	ssh -q "$REMOTE" 'lpstat' 2>/dev/null | while read -r LINE; do
+		echo -n "pstat: request id ${LINE%%\ *} sent at "
+		date "--date=$(echo "$LINE" | tr -s ' ' | \ cut '-d ' -f4-)" \
+			"+%T on %A, %B %d"
+	done
+elif [ "$1" = 'cancel' ]; then
+	ssh -q "$REMOTE" "cancel ${2:--a}" 2>/dev/null
+	[ $? -ne 0 ] && error 'print: unable to cancel requests' $?
+	echo 'print: requests cancelled' >&2
+else
+	ssh -q "$REMOTE" "mkdir -p $PRDEST" 2>/dev/null
+	[ $? -ne 0 ] && error "print: unable to mkdir $PRDEST" $?
 
-case "$1" in
-	stat)
-		ssh -q "$REMOTE" 'lpstat' 2>/dev/null | while read -r LINE; do
-			echo -n "pstat: request id ${LINE%%\ *} sent at "
-			date "--date=$(echo "$LINE" | tr -s ' ' | \
-				cut '-d ' -f4-)" "+%T on %A, %B %d"
-		done
-		;;
-	cancel)
-		REQ="$2"
-		[ -z "$REQ" ] && REQ='-a'
+	scp "$1" "$REMOTE:$PRDEST" >/dev/null
+	[ $? -ne 0 ] && error 'print: unable to transfer document' $?
+	echo "print: document transferred to $PRDEST/$1" >&2
 
-		ssh -q "$REMOTE" "cancel $REQ" 2>/dev/null
-		[ $? -ne 0 ] && error 'print: unable to cancel requests' $?
-		echo 'print: requests cancelled' >&2
-		;;
-	*.pdf)
-		ssh -q "$REMOTE" "mkdir -p $DEST" 2>/dev/null
-		[ $? -ne 0 ] && error "print: unable to mkdir $DEST" $?
-
-		scp "$1" "$REMOTE:$DEST" >/dev/null
-		[ $? -ne 0 ] && error 'print: unable to transfer document' $?
-		echo "print: document transferred to $DEST/$1" >&2
-
-		REQ="$(ssh -q "$REMOTE" "lp -o media=Letter $DEST/$(basename \
-			"$1")" 2>/dev/null | cut '-d ' -f4)"
-		[ $? -ne 0 ] && error 'print: unable to print document' $?
-		echo "print: printing with request id $REQ" >&2
-		;;
-	*)
-		error 'usage: print [cmd] [opt...]'
-		;;
-esac
+	REQ="$(ssh -q "$REMOTE" "lp -o media=Letter $PRDEST/${1##*/}" \
+		2>/dev/null)"
+	[ $? -ne 0 ] && error 'print: unable to print document' $?
+	echo "print: printing with request id ${REQ##* }" >&2
+fi
--- xinitrc.sh
+++ xinitrc.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-# xinitrc - xinit client config
+# xinitrc - xorg client
 # Copyright (C) 2022 ArcNyxx
 # see LICENCE file for licensing information
 
 xrandr --output 'eDP-1'    --mode '1920x1080' \
        --output 'HDMI-1-1' --mode '1920x1080' --left-of 'eDP-1'
-
 xwallpaper --zoom images/etc/wallpaper.png
 xcompmgr -n &
 while stext; do
 	sleep 0.25
 done &
 
-[ "$#" -gt 0] && exec $@
+[ $# -gt 0] && exec $@
 exec swim
