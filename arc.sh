0be0a469868ceddff19b0764f4e8b363877b8d85
Doggo <fearlessdoggo21@vivaldi.net>
2022-04-27 10:41:19 -0500

    add arc script

---
+++ arc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# arc - archive git repositories
+# Copyright (C) 2022 ArcNyxx
+# see LICENCE file for licensing information
+
+recurse() {
+	if [ "$(ls "${1:-.}/.git")" ]; then
+		tar -cvzf "${1:-$PWD}.tar.gz" "${1:-.}"
+	else
+		ls "${1:-.}" | while read -r FOLD; do
+			recurse "${1:-.}/$FOLD"
+		done
+	fi
+}
+
+recurse
