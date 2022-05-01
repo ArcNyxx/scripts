25a0b04953c42e6965fddf57797c5816a3cfc1e1
Doggo <fearlessdoggo21@vivaldi.net>
2022-04-30 23:30:35 -0500

    fix pswd script

--- pswd.sh
+++ pswd.sh
@@ -7,7 +7,7 @@ error() {
 	echo "$1" ; exit "${2:-1}"
 }
 
-[ -z "$PSWD" ]   && error 'pswd: $PSWD variable unset'
+[ -z "$PSWD" ] && error 'pswd: $PSWD variable unset'
 if [ ! -f "$PSWD" ]; then
 	mkdir -p "${PSWD%/*}" && touch "$PSWD" 2>/dev/null && age -p "$PSWD"
 	[ $? -ne 0 ] && error 'pswd: unable to make $PSWD file'
@@ -16,9 +16,8 @@ fi
 case "$1" in
 	add)
 		[ $# -ne 4 ] && error 'usage: pswd add [site] [user] [pass]'
-		READ="$(age -d "$PSWD" 2>/dev/null || error \
-			'pswd: unable to read $PSWD file')"
-		READ="$(readf)"
+		READ="$(age -d "$PSWD" 2>/dev/null)"
+		[ -z "$READ" ] && error 'pswd: unable to read $PSWD file'
 		echo -e "$READ\n$2 $3 $4" | sort -u | column -t | \
 			age -e -p -o "$PSWD"
 		;;
@@ -29,8 +28,8 @@ case "$1" in
 	remove)
 		[ $# -ne 2 -a $# -ne 3 ] && error \
 			'usage: pswd remove [site] [user]'
-		READ="$(age -d "$PSWD" 2>/dev/null || error \
-			'pswd: unable to read $PSWD file')"
+		READ="$(age -d "$PSWD" 2>/dev/null)"
+		[ -z "$READ" ] && error 'pswd: unable to read $PSWD file'
 
 		GREP="$(echo "$READ" | awk "/^$2/ ${3:+&& /  $3  /}")"
 		[ "$(echo "$GREP" | wc -l)" -ne 1 ] && error \
